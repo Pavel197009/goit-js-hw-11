122ee34df9eb13e2a74771f0edcb86e0b12477c7
Pavel197009 <parutman@gmail.com>
2026-06-11 15:29:27 +0300

    Delete src/css/js/refs.js

--- src/css/js/refs.js
+++
@@ -1,8 +0,0 @@
-export const refs = {
-    form: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery'),
-    body: document.querySelector('body'),
-    searchInput: document.querySelector('.search-form-input'),
-    searchBtn: document.querySelector('.search-form-btn'),
-    loadingText: document.querySelector('.loading-text'),
-  };
