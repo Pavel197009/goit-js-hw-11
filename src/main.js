19d2dce4295e0b10d2fc42ecd3aa76aef573bf48
Pavel197009 <parutman@gmail.com>
2026-06-11 17:25:49 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 //import { createMarkup } from './js/render-functions';
-import { getPhotos } from './js/PixabayAPI';
-import { createMarkup } from './js/render-functions';
+import { getImagesByQuery } from './js/PixabayAPI';
+import { clearGallery, createGallery, hideLoader, showLoader } from './js/render-functions';
 import { refs } from './js/refs';
 
 //const form = document.querySelector('.search-form');
@@ -22,19 +19,15 @@ function handleSearchPhoto(e) {
         iziToast.error({ position: 'topRight', message: 'Search text is empty. Enter the text, please' });
     } */
     let searchString = e.currentTarget.elements.search_text.value.split(" ").join("+");
-    refs.gallery.innerHTML = "";
-    refs.loadingText.style.visibility = 'visible';
+    clearGallery();
+    showLoader();
 //    const pixabay = new PixabayAPI;
-    let data = getPhotos(searchString).then(res => {
+    let data = getImagesByQuery(searchString).then(res => {
         if (!res.data.total) {
             iziToast.error({ position: 'topRight', message: `Sorry, there are no images matching your search query. Please try again!` });
         } else {
-            const markup = createMarkup(res.data.hits);
-            refs.gallery.innerHTML = markup;
-            const lightbox = new SimpleLightbox('.gallery a', {
-                captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
-            });
+            createGallery(res.data.hits);
         }
-        refs.loadingText.style.visibility = 'hidden';
+        hideLoader();
     });    
 }
