77f31f8c1a57833f225d05490d241b9b3555d947
Pavel197009 <parutman@gmail.com>
2026-06-12 13:25:42 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,31 +3,29 @@ import "izitoast/dist/css/iziToast.min.css";
 
 //import { createMarkup } from './js/render-functions';
 import { getImagesByQuery } from './js/PixabayAPI';
-import { clearGallery, createGallery, hideLoader, showLoader } from './js/render-functions';
+import { clearGallery, createGallery, hideLoader, showLoader, createLightBox } from './js/render-functions';
 import { refs } from './js/refs';
 
-//const form = document.querySelector('.search-form');
 refs.form.addEventListener("submit", handleSearchPhoto);
 
-
+createLightBox();
   
 function handleSearchPhoto(e) {
     e.preventDefault();
-    console.log(e.currentTarget.elements.search_text.value, e.currentTarget.elements.search_text.value.length);
-/*    if (e.currentTarget.elements.search_text.value.length === 0)
+    if (e.currentTarget.elements.search_text.value.trim().length === 0)        // если строка поиска пустая или пробелы
     {
         iziToast.error({ position: 'topRight', message: 'Search text is empty. Enter the text, please' });
-    } */
-    let searchString = e.currentTarget.elements.search_text.value.split(" ").join("+");
-    clearGallery();
-    showLoader();
-//    const pixabay = new PixabayAPI;
-    let data = getImagesByQuery(searchString).then(res => {
-        if (!res.data.total) {
+        return;
+    };
+    let searchString = e.currentTarget.elements.search_text.value.trim().split(" ").join("+");  // преобразование строки поиска для HTTP-запроса
+    clearGallery();                                                             // очистка галереи
+    showLoader();                                                               // светим лоадер
+    let data = getImagesByQuery(searchString).then(res => {                     // обработчик промиса 
+        if (!res.data.total) {                                                  // если ничего не вернули из запроса
             iziToast.error({ position: 'topRight', message: `Sorry, there are no images matching your search query. Please try again!` });
-        } else {
+        } else {                                                                // иначе строим галерею с картинками
             createGallery(res.data.hits);
         }
-        hideLoader();
+        hideLoader();                                                           // гасим лоадер после загрузки картинок
     });    
 }
