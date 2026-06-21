bbd0939e9a8c498dab96d3ad8b97b05f9908b3e0
Pavel197009 <parutman@gmail.com>
2026-06-21 23:13:21 +0300

    js commit

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export const refs = {
-    form: document.querySelector('.search-form'),
+    form: document.querySelector('.form'),
     gallery: document.querySelector('.gallery'),
     body: document.querySelector('body'),
     searchInput: document.querySelector('.search-form-input'),
